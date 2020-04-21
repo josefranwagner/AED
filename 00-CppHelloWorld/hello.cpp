8a11969e352c6b06980f6e1b2fe71a41b6acf265
josefranwagner <63469178+josefranwagner@users.noreply.github.com>
2020-04-21 12:22:19 -0300

    Add files via upload

---
+++ 00-CppHelloWorld/hello.cpp
@@ -0,0 +1,12 @@
+/*
+Programa: Hello, World!
+Objetivo: Imprimir en pantalla el mensaje 'Hello, World!'
+Autor: Jose Francisco Wagner
+Ultima modificación: 20200421
+*/
+
+#include <iostream>
+
+int main (){
+	std::cout << "Hello, World!\n";
+}
