e99bc488621d97476ddd13a2acfb234cae559b15
josefranwagner <63469178+josefranwagner@users.noreply.github.com>
2020-04-26 19:25:55 -0300

    Add files via upload

---
+++ 01-Adición/adicion.cpp
@@ -0,0 +1,19 @@
+/*
+Programa: Adicion
+Objetivo: Devuelve la suma de dos numeros ingresados por el usuario
+Autor: Jose Francisco Wagner
+Ultima modificación: 20200426
+*/
+
+#include <iostream>
+
+int main (){
+	std::cout << "Ingrese un primer valor a sumar: ";
+    int PrimerNum;
+    std::cin >> PrimerNum;
+    std::cout << "Ingrese un segundo valor a sumar: ";
+    int SegundoNum;
+    std::cin >> SegundoNum;
+    std::cout << "RESULTADO: ";
+    std::cout << PrimerNum+SegundoNum;
+}
