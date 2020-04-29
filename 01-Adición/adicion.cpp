f29eb6f0e0fb5d47ef5104cf39682751958a667d
josefranwagner <63469178+josefranwagner@users.noreply.github.com>
2020-04-29 16:23:46 -0300

    Update adicion.cpp

--- 01-Adición/adicion.cpp
+++ 01-Adición/adicion.cpp
@@ -9,11 +9,11 @@ Ultima modificación: 20200426
 
 int main (){
 	std::cout << "Ingrese un primer valor a sumar: ";
-    int PrimerNum;
-    std::cin >> PrimerNum;
-    std::cout << "Ingrese un segundo valor a sumar: ";
-    int SegundoNum;
-    std::cin >> SegundoNum;
-    std::cout << "RESULTADO: ";
-    std::cout << PrimerNum+SegundoNum;
+	int PrimerNum;
+	std::cin >> PrimerNum;
+	std::cout << "Ingrese un segundo valor a sumar: ";
+	int SegundoNum;
+	std::cin >> SegundoNum;
+	std::cout << "RESULTADO: ";
+	std::cout << PrimerNum+SegundoNum;
 }
