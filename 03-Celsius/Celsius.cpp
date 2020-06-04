0ddaba86c057e2a2683a229be0914eb249e5c136
josefranwagner <63469178+josefranwagner@users.noreply.github.com>
2020-06-04 02:15:49 -0300

    Add files via upload

---
+++ 03-Celsius/Celsius.cpp
@@ -0,0 +1,36 @@
+/*
+Programa: Función Celsius
+Objetivo: Desarrollar una función que, dada una magnitud Farehnheit, calcule su equivalente en Celsius
+Autor: Jose Francisco Wagner
+Ultima modificación: 20200604
+*/
+
+#include <cassert>
+#include <iostream>
+
+// Prototipos de funciones
+
+double Celsius (double);
+bool AreNear (double a, double b, double = 0.001);
+
+// BLOQUE PRINCIPAL
+
+int main (){
+    assert (AreNear(0,Celsius(32.00),0.001));           // 32° F = 0° C | 0-0 <= 0.001
+    assert (AreNear(32.00,Celsius(89.6),0.001));        // 89.6° F = 32° C | 32.00 - 32.00 <= 0.001
+    assert (AreNear(-32.00,Celsius(-25.6),0.001));      // -25.6° F = -32° C | -32.00 - (-32.00) <= 0.001
+    assert (AreNear(-32.001,Celsius(-25.6),0.001));     // -25.6° F = -32° C | -32.001 - (-32.00) <= 0.001
+    assert (AreNear(-32.0001,Celsius(-25.6),0.001));    // -25.6° F = -32° C | -32.00 - (-32.0001) = 0.0001
+    assert (AreNear(-32.002,Celsius(-25.6),0.001));     // -25.6° F = -32° C | -32.00 - (-32.002) = 0.002
+}
+
+// Definición de la función
+
+double Celsius (double x){return
+    (5.00/9.00)*(x-32);
+}
+
+bool AreNear (double a, double b, double tolerancia){return
+    a >= b? (a-b) <= tolerancia
+    : (b-a) <= tolerancia;
+}
